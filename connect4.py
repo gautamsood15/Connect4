31ba0b0270c13c91f64c6ddd7b8e8f1218c54d59
gautamsood15 <gauty22@gmail.com>
2021-07-14 06:32:17 +0530

    Adding new import

--- connect4.py
+++ connect4.py
@@ -1,18 +1,1 @@
-import numpy as np 
-
-
-def create_board():
-	board = np.zero((6,7))
-	return board
-
-board = create_board()
-game_over = False
-turn = 0
-
-while not game_over:
-
-	if turn == 0:
-		selection = int(input("Player 1 make your selection (0-6):"))
-
-		print(selection)
-		print(type(selection))
+import numpy as np
