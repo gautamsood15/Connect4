b89c12de9de1effdaf16e99cbbf0f2a26b7474ba
gautamsood15 <gauty22@gmail.com>
2021-07-15 07:56:04 +0530

    Preventing screen popup + adding event type mousedown

--- connect4.py
+++ connect4.py
@@ -77,33 +77,33 @@ while not game_over:
 		if event.type == pygame.MOUSEBUTTONDOWN:
 
 
-	# Ask for Player 1 Input
+			# Ask for Player 1 Input
 
-	if turn == 0:
-		col = int(input("Player 1 Make your selection (0-6):"))
+			if turn == 0:
+				col = int(input("Player 1 Make your selection (0-6):"))
 
-		if is_valid_location(board, col):
-			row = get_next_open_row(board, col)
-			drop_piece(board, row, col, 1)
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, 1)
 
-			if winning_move(board, 1):
-				print("PLAYER 1 Wins!!!! Congrats!!!")
-				game_over = True
+					if winning_move(board, 1):
+						print("PLAYER 1 Wins!!!! Congrats!!!")
+						game_over = True
 
-	# Ask for Player 2 Input
+			# Ask for Player 2 Input
 
-	else:
-		col = int(input("Player 2 Make your selection (0-6):"))
+			else:
+				col = int(input("Player 2 Make your selection (0-6):"))
 
-		if is_valid_location(board, col):
-			row = get_next_open_row(board, col)
-			drop_piece(board, row, col, 2)
+				if is_valid_location(board, col):
+					row = get_next_open_row(board, col)
+					drop_piece(board, row, col, 2)
 
-			if winning_move(board, 1):
-				print("PLAYER 2 Wins!!!! Congrats!!!")
-				game_over = True
+					if winning_move(board, 1):
+						print("PLAYER 2 Wins!!!! Congrats!!!")
+						game_over = True
 
-	print_board(board)
+			print_board(board)
 
-	turn += 1
-	turn = turn % 2
+			turn += 1
+			turn = turn % 2
