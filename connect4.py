dee598bbd3869f23ab1fb6db379cd817b18753e3
gautamsood15 <gauty22@gmail.com>
2021-07-18 14:24:05 +0530

    adding loop for negative slope win

--- connect4.py
+++ connect4.py
@@ -44,7 +44,10 @@ def winning_move(board, piece):
 				return True
 
 	# Check negatively sloped diagonals
-
+	for c in range(COLUMN_COUNT-3):
+		for r in range(ROW_COUNT-3):
+			if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+				return True
 
 
 board = create_board()
