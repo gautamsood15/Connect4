80cd42be2abc11741a15ddc21d244da0b82242e5
gautamsood15 <gauty22@gmail.com>
2021-07-20 12:14:20 +0530

    Adding winning move for vertical locations for win

--- connect4.py
+++ connect4.py
@@ -32,9 +32,9 @@ def winning_move(board, piece):
 				return True
 
 	# Check vertical locations for win
-	for c in range(COLUMN_COUNT-3):
-		for r in range(ROW_COUNT):
-			if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+	for c in range(COLUMN_COUNT):
+		for r in range(ROW_COUNT-3):
+			if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
 				return True
 
 
