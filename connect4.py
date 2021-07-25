63ffcdce6a301ba9c0158592ef4cf9e8d35d4730
gautamsood15 <gauty22@gmail.com>
2021-07-25 11:55:03 +0530

    drawing red drop piece

--- connect4.py
+++ connect4.py
@@ -109,6 +109,8 @@ while not game_over:
 		if event.type == pygame.MOUSEMOTION:
 			pygame.draw.rect(screen, BLACK, (0,0,width,SQUARESIZE))
 			posx = event.pos[0]
+			if turn == 0:
+				pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
 
 
 		if event.type == pygame.MOUSEBUTTONDOWN:
