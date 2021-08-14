89b2ec3c9e1961bb20db20734d3d902fcc3843fd
gautamsood15 <gauty22@gmail.com>
2021-08-14 12:35:31 +0530

    condition ofr 4 pieces

--- connect4.py
+++ connect4.py
@@ -107,6 +107,7 @@ def score_position(board, piece):
 	for r in range(ROW_COUNT-3):
 		for c in range(COLUMN_COUNT-3):
 			window = [board[r+3-i][c+i] for i in range (WINDOW_LENGTH)]
+			if window.count(piece) == 4:
 
 	return score
 
