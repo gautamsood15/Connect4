947331ed78de12f818cc125d4c6e753f0ac5f197
gautamsood15 <gauty22@gmail.com>
2021-08-15 12:37:12 +0530

    score for 10 pieces

--- connect4.py
+++ connect4.py
@@ -110,7 +110,7 @@ def score_position(board, piece):
 			if window.count(piece) == 4:
 				score += 100
 			elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-
+				score += 10
 
 	return score
 
