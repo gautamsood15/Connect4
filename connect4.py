52902e6e39c44be8b17c54435c68715f3f32299e
gautamsood15 <gauty22@gmail.com>
2021-08-22 17:51:08 +0530

    check for terminal and depth condition minmax

--- connect4.py
+++ connect4.py
@@ -132,7 +132,8 @@ def is_terminal_node(board):
 def minimax(board, depth, maximizingPlayer):
 	valid_locations = get_valid_locations(board)
 	is_terminal = is_terminal_node(board)
-	
+	if depth == 0 or is_terminal:
+		
 
 
 
