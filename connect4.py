da0d0f3f228d37a6c2ec53d131cd377bace4160b
gautamsood15 <gauty22@gmail.com>
2021-08-23 18:40:02 +0530

    dumcommmm

--- connect4.py
+++ connect4.py
@@ -141,8 +141,9 @@ def minimax(board, depth, maximizingPlayer):
 			else:   # Game is over, no more valid moves
 				return 0
 		else:		#Depth is zero
-			
 
+
+		
 def get_valid_locations(board):
 	valid_locations = []
 	for col in range(COLUMN_COUNT):
