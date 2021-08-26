95474aed20c3eb0777f95743d6aca4eed607c3e5
gautamsood15 <gauty22@gmail.com>
2021-08-26 19:45:28 +0530

    correcting fxn name

--- connect4.py
+++ connect4.py
@@ -149,7 +149,7 @@ def minimax(board, depth, maximizingPlayer):
 			row = get_next_open_row(board, col)
 			b_copy = board.copy()
 			drop_piece(b_copy, row, col, AI_PIECE)
-			new_score = max(value, minmax())
+			new_score = max(value, minimax())
 
 
 def get_valid_locations(board):
