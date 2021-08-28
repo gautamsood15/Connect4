5bc8d2c096495b2b2e91b48a9da9ccdf83d0cf40
gautamsood15 <gauty22@gmail.com>
2021-08-28 19:57:04 +0530

    removing min function

--- connect4.py
+++ connect4.py
@@ -149,7 +149,7 @@ def minimax(board, depth, maximizingPlayer):
 			row = get_next_open_row(board, col)
 			b_copy = board.copy()
 			drop_piece(b_copy, row, col, AI_PIECE)
-			new_score = max(value, minimax(b_copy, depth-1, False))
+			new_score = minminimax(b_copy, depth-1, False)
 			return new_score
 
 	else:	# Minimizing Player
@@ -158,7 +158,7 @@ def minimax(board, depth, maximizingPlayer):
 			row = get_next_open_row(board, col)
 			b_copy = board.copy()
 			drop_piece(b_copy, row, col, PLAYER_PIECE)
-			new_score = min(value, minimax(b_copy, depth-1, True))
+			new_score = minimax(b_copy, depth-1, True)
 			return new_score
 
 
