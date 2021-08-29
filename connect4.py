ea73386e8f09eca8b44ae1a9e1b17972026192f9
gautamsood15 <gauty22@gmail.com>
2021-08-29 20:09:02 +0530

    getting value for new score

--- connect4.py
+++ connect4.py
@@ -151,7 +151,7 @@ def minimax(board, depth, maximizingPlayer):
 			drop_piece(b_copy, row, col, AI_PIECE)
 			new_score = minimax(b_copy, depth-1, False)
 			if new_score > value:
-				
+				value = new_score
 			return new_score
 
 	else:	# Minimizing Player
