8ee962f28fd9baaaff913484e8959e809f8be0ee
gautamsood15 <gauty22@gmail.com>
2021-09-02 17:02:50 +0530

    Adding score for 3 in a row

--- connect4.py
+++ connect4.py
@@ -78,7 +78,7 @@ def evaluate_window(window, piece):
 	if window.count(piece) == 4:
 		score += 100
 	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-		score += 10
+		score += 5
 	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
 		score += 5
 
